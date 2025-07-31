ef663ebc57e97ff19425b7b82bcccf5703af21ac
saulo.rodrigues <saulohrodrigues@gmail.com>
2025-07-31 10:21:12 -0300

    Iniciando o projeto de estudo.

---
+++ testes_automatizados/src/test/java/com/testes/testes_automatizados/TestesAutomatizadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testes.testes_automatizados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestesAutomatizadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
